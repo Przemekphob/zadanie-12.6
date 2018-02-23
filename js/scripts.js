96f763ed8d310319d6ab3dd94f2692a60d817c7b
Przemek Mazur <Przemek1713@gmail.com>
2018-02-23 23:34:02 +0100

    added filter of data

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,39 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 
 var countriesList = $('#countries');
-
+var countryArray = [];
 $('#search').click(searchCountries);
 
 function searchCountries() {
  	var countryName = $('#country-name').val();
 	if(!countryName.length) countryName = 'Poland';
-	$.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
+	if (countryArray.indexOf(countryName) === -1) {
+		countryArray.push(countryName);
+		$.ajax({
+	  		url: url + countryName,
+	  		method: 'GET',
+	  		success: showCountriesList						
+	  	});
+	} else {
+		console.log("This item already exists");
+	} 
+
+	console.log(countryArray);
+
 }
 
 function showCountriesList(resp) {
-    countriesList.empty();
 	resp.forEach(function(item) {
-	   	$('<li>').text('Country: ' + item.name).appendTo(countriesList);
-	   	$('<li>').text('Background information: ').addClass('thirdchild').appendTo(countriesList);
-	   	$('<li>').text('Currencies: ' + item.currencies).appendTo(countriesList);
-		$('<li>').text('Population: ' + item.population).appendTo(countriesList);
-		$('<li>').text('Region: ' + item.region).appendTo(countriesList);
-	  	$('<li>').text('Area: ' + item.area).appendTo(countriesList); 	
+		var newCountry = $('<ul>').addClass("newCountry");
+	   	$('<li>').text('Country: ' + item.name).appendTo(newCountry);
+	   	$('<li>').text('Background information: ').addClass('nodecor').appendTo(newCountry);
+	   	$('<li>').text('Currencies: ' + item.currencies).appendTo(newCountry);
+		$('<li>').text('Population: ' + item.population).appendTo(newCountry);
+		$('<li>').text('Region: ' + item.region).appendTo(newCountry);
+	  	$('<li>').text('Area: ' + item.area).appendTo(newCountry); 
+	  	newCountry.appendTo(countriesList);	
 
-		
+
 	});    
 }
 
